0b08cfb81a6f7e88df6e0a955b997420db0a7924
finny <phineas.ehrlich2026@gbwl.org>
2026-02-11 14:38:14 +0000

    added lists, dicts

--- app.py
+++ app.py
@@ -8,8 +8,20 @@ app = Flask(__name__)
 # using the app.route decorator to map the URL
 @app.route('/')
 def index():
-    name_data = None
-    return render_template('index.html', name=name_data)
+    name_data = 'Jimothy Bumblebottoms'
+    year_data = 2026
+    favorites_list = ['Glarfle', 'Sprocket', 'Zibble', 'Wobble', 'Flibber', 'Gizmo', 'Blimgog', 'Snorfle', 'Quibble', 'Dingle', 'Zazzle']
+    ratings_dict = {
+        'Mango': 'I rate this mango, a 3/10.',
+        'Pineapple': 'This pineapple is a 4/10.',
+        'Strawberry': 'I do not like this strawberry, it is a 1/10.',
+        'Watermelon': 'This watermelon is not good. I give it a 2/10.',
+        'Blueberry': 'No, this blueberry is a 0/10. I do not like it at all.'
+    }
+
+    # name is how we refer to it in the HTML template,
+    # name_data is the variable declared here in python
+    return render_template('index.html', name=name_data, year=year_data, favorites=favorites_list, ratings=ratings_dict)
 
 
 
